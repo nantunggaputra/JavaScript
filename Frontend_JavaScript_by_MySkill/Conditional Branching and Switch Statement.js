e1e023064857431bae00c7a98ab3dd5feb272ae8
nantunggaputra <nantunggaputra@github.com>
2024-02-17 20:48:05 +0700

    Create .js

---
+++ Frontend_JavaScript_by_MySkill/Conditional Branching and Switch Statement.js
@@ -0,0 +1,20 @@
+// conditional operator
+let age = 10;
+var identityCardAllowed = age >= 17 ? true : false; // detailed version
+console.log(identityCardAllowed);
+var identityCardAllowed = age >= 17; // short-hand version
+console.log(identityCardAllowed);
+
+// boolean conversion
+if (0) {
+  console.log(false); // not executed
+}
+if (1) {
+  console.log(true); // executed
+}
+var year = 2024;
+let cond = year == 2024;
+if (cond) {
+  // executed if year == 2024
+  console.log("Happy New Year");
+}
