1c592c579f937a295500b7d0d8db00d5be7821fd
nantunggaputra <nantunggaputra@github.com>
2024-02-25 11:40:01 +0700

    Create .js .html .json .prettierrc

--- Frontend_JavaScript_by_MySkill/Document Object Model: Part 1.js
+++ Frontend_JavaScript_by_MySkill/Document Object Model: Part 1.js
@@ -29,9 +29,7 @@ const elementIntroductionNothing = document.getElementById("nointroduction");
 console.log("what is in the id :");
 console.log(elementIntroduction);
 // add element from id="introduction" into element which id="demonstration"
-document.getElementById("demonstration").innerHTML =
-  "The text from the introduction nothing paragraph is: " +
-  elementIntroduction.innerHTML;
+document.getElementById("demonstration").innerHTML = "The text from the introduction nothing paragraph is: " + elementIntroduction.innerHTML;
 // // if no element which id="nointroduction"
 // document.getElementById("demonstration").innerHTML =
 //   "The text from the introduction paragraph is: " +
@@ -39,21 +37,36 @@ document.getElementById("demonstration").innerHTML =
 // // Result = Uncaught TypeError: elementIntroductionNothing is null
 
 // get elements by class
-const elementIntroductionClass =
-  document.getElementsByClassName("introduction_class");
+const elementIntroductionClass = document.getElementsByClassName("introduction_class");
 // what is in the class
 console.log("what is in the class :");
 console.log(elementIntroductionClass);
 // add element from first class="introduction_class" into element which id="demonstration_class_first"
-document.getElementById("demonstration_class_first").innerHTML =
-  "The text from the first introduction_class paragraph is: " +
-  elementIntroductionClass[0].innerHTML;
+document.getElementById("demonstration_class_first").innerHTML = "The text from the first introduction_class paragraph is: " + elementIntroductionClass[0].innerHTML;
 // add element from second class="introduction_class" into element which id="demonstration_class_second"
-document.getElementById("demonstration_class_second").innerHTML =
-  "The text from the second introduction_class paragraph is: " +
-  elementIntroductionClass[1].innerHTML;
+document.getElementById("demonstration_class_second").innerHTML = "The text from the second introduction_class paragraph is: " + elementIntroductionClass[1].innerHTML;
 // // if no third class which class="introduction_class"
 // document.getElementById("demonstration_class_second").innerHTML =
 //   "The text from the third introduction_class paragraph is: " +
 //   elementIntroductionClass[2].innerHTML;
 // // Result = Uncaught TypeError: elementIntroductionClass[2] is undefined
+
+// get elements by tag name
+const elementIntroductionTagNameP = document.getElementsByTagName("p");
+const elementIntroductionTagNameLI = document.getElementsByTagName("li");
+// what is in the tag
+console.log("what is in the tag :");
+console.log(elementIntroductionTagNameP);
+console.log(elementIntroductionTagNameLI);
+// add element from first p tag into element which id="demonstration_tag_name_p"
+document.getElementById("demonstration_tag_name_p").innerHTML = "The text from the first p tag is: " + elementIntroductionTagNameP[0].innerHTML;
+// add element from first li tag into element which id="demonstration_tag_name_li"
+document.getElementById("demonstration_tag_name_li").innerHTML = "The text from the first li tag is: " + elementIntroductionTagNameLI[0].innerHTML;
+
+// get element by query selector all
+const elementIntroductionSelector = document.querySelectorAll("p.introduction_class");
+// what is in the selector
+console.log("what is in the selector :");
+console.log(elementIntroductionSelector);
+// add element from first p tag which class="introduction_class" into element which id="demonstration_selector_p_introduction_class"
+document.getElementById("demonstration_selector_p_introduction_class").innerHTML = "The text from the first selector p.introduction_class is: " + elementIntroductionSelector[0].innerHTML;
