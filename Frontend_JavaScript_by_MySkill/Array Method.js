54b6f2202ad46500ada77872b52598d15e9db432
nantunggaputra <nantunggaputra@github.com>
2024-03-03 19:24:10 +0700

    Create .js

--- Frontend_JavaScript_by_MySkill/Array Method.js
+++ Frontend_JavaScript_by_MySkill/Array Method.js
@@ -57,3 +57,38 @@ let slicedNewFruitsFromDurian = fruits.slice(2); // slice element from index 2
 console.log(slicedNewFruitsFromDurian); // ['Durian', 'Strawberry', 'Blueberry']
 let slicedNewFruitsFromBananaToStrawberry = fruits.slice(1, 4); // slice element from index 1 until before index 4
 console.log(slicedNewFruitsFromBananaToStrawberry); // ['Banana', 'Durian', 'Strawberry']
+
+// sort in array
+let numb = [3000, 1, 25, 633333, 100, 70, 340, 4, 10, 790];
+// example A-Z / ascending
+numb.sort();
+console.log(numb); // [1, 10, 100, 25, 3000, 340, 4, 633333, 70, 790]
+fruits.sort();
+console.log(fruits); // ['Banana', 'Blueberry', 'Durian', 'Orange', 'Strawberry']
+// example Z-A / descending
+numb.reverse();
+console.log(numb); // [790, 70, 633333, 4, 340, 3000, 25, 100, 10, 1]
+fruits.reverse();
+console.log(fruits); // ['Strawberry', 'Orange', 'Durian', 'Blueberry', 'Banana']
+// example 0-9n / ascending
+numb.sort(function (a, b) {
+	return a - b;
+});
+console.log(numb); // [1, 4, 10, 25, 70, 100, 340, 790, 3000, 633333]
+// example 9n-0 / descending
+numb.sort(function (a, b) {
+	return b - a;
+});
+console.log(numb); // [633333, 3000, 790, 340, 100, 70, 25, 10, 4, 1]
+
+// sort in array's object
+cars = [
+	{ type: "Toyota", year: 2019 },
+	{ type: "BMW", year: 2005 },
+	{ type: "Lamborghini", year: 1997 },
+	{ type: "Ferrari", year: 2024 },
+];
+// example 0-9n / ascending by year
+cars.sort(function (a, b) {
+	return a.year - b.year;
+});
