10da0eeb99831d86c4a81f67cc03ab6595ea8e7c
nantunggaputra <nantunggaputra@github.com>
2024-03-12 00:14:06 +0700

    Create .js

--- Frontend_JavaScript_by_MySkill/JavaScript Dates.js
+++ Frontend_JavaScript_by_MySkill/JavaScript Dates.js
@@ -25,3 +25,37 @@ dateString = new Date("Nove, 12 2006, 4:00 pm");
 console.log(dateString); // the date is same with before
 dateString = new Date("12-Nove-2006, 4:00 pm");
 console.log(dateString); // the date is same with before
+
+// Dates Method
+
+// example new Date()
+console.log(d.toString()); // Tue Mar 12 2024 00:04:08 GMT+0700 (Western Indonesia Time)
+console.log(d.toUTCString()); // Mon, 11 Mar 2024 17:00:00 GMT
+console.log(d.toDateString()); // Tue Mar 12 2024
+console.log(d.toISOString()); // 2024-03-11T17:00:00.000Z
+
+// Dates Format
+
+// example new Date("YYYY-MM-DD") ISO Date
+let dateFormat = new Date("2024-03-12");
+console.log(dateFormat); // Tue Mar 12 2024 00:00:00 GMT+0700 (Western Indonesia Time)
+dateFormat = new Date("2024-03");
+console.log(dateFormat); // Fri Mar 01 2024 00:00:00 GMT+0700 (Western Indonesia Time)
+dateFormat = new Date("2024");
+console.log(dateFormat); // Mon Jan 01 2024 07:00:00 GMT+0700 (Western Indonesia Time)
+dateFormat = new Date("2024-03-11T17:00Z");
+console.log(dateFormat); // Tue Mar 12 2024 00:00:00 GMT+0700 (Western Indonesia Time)
+
+// example new Date("MM/DD/YYYY") Short Date
+dateFormat = new Date("01/31/2025");
+console.log(dateFormat); // Fri Jan 31 2025 00:00:00 GMT+0700 (Western Indonesia Time)
+
+// example new Date("Mth DD YYYY")
+dateFormat = new Date("Jan 31 2025");
+console.log(dateFormat); // Fri Jan 31 2025 00:00:00 GMT+0700 (Western Indonesia Time)
+dateFormat = new Date("31 Jan 2025");
+console.log(dateFormat); // Fri Jan 31 2025 00:00:00 GMT+0700 (Western Indonesia Time)
+dateFormat = new Date("31 JANua 2025");
+console.log(dateFormat); // Fri Jan 31 2025 00:00:00 GMT+0700 (Western Indonesia Time)
+dateFormat = new Date("31, Januari, 2025");
+console.log(dateFormat); // Fri Jan 31 2025 00:00:00 GMT+0700 (Western Indonesia Time)
