1987f892fa17ab7d0a1982ccc5b77eed17e7da57
nantunggaputra <nantunggaputra@github.com>
2024-03-28 22:41:57 +0700

    Create Weather App .js .html .css

--- Frontend_JavaScript_by_MySkill/Mini Project Weather App/index.js
+++ Frontend_JavaScript_by_MySkill/Mini Project Weather App/index.js
@@ -15,34 +15,37 @@ const UrlAPI = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 const keyAPI = "9731f29d3257cec279a800e494b516cc";
 
 // async function
-async function checkWeather(inputCityName) {
-	const response = await fetch(UrlAPI + inputCityName + `&appid=${keyAPI}`);
-	let data = await response.json();
-	// console.log(data);
-	setTimeout(() => {
-		if (!response.ok) {
-			weather.src = "./weather/umbrella.svg";
-			data.name = "Unknown Place / Not a City";
-			city.innerHTML = data.name;
-			alert("Invalid City Name" + `\n` + "You entered the wrong city name unfortunately. Please try again.");
-		} else {
-			if (data.weather[0].main == "Clear") {
-				weather.src = "./weather/sun.svg";
-			} else if (data.weather[0].main == "Snow") {
-				weather.src = "./weather/cloud-snow.svg";
-			} else if (data.weather[0].main == "Rain") {
-				weather.src = "./weather/cloud-rain.svg";
-			} else if (data.weather[0].main == "Drizzle") {
-				weather.src = "./weather/cloud-drizzle.svg";
-			} else {
-				weather.src = "./weather/cloud.svg";
-			}
-			city.innerHTML = data.name;
-			temp.innerHTML = Math.round(data.main.temp) + "°C";
-			humidity.innerHTML = data.main.humidity + "%";
-			wind.innerHTML = data.wind.speed + " km/h";
-		}
-	}, 500);
+function checkWeather(inputCityName) {
+    fetch(UrlAPI + inputCityName + `&appid=${keyAPI}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Invalid City Name');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.weather[0].main == "Clear") {
+                weather.src = "./weather/sun.svg";
+            } else if (data.weather[0].main == "Snow") {
+                weather.src = "./weather/cloud-snow.svg";
+            } else if (data.weather[0].main == "Rain") {
+                weather.src = "./weather/cloud-rain.svg";
+            } else if (data.weather[0].main == "Drizzle") {
+                weather.src = "./weather/cloud-drizzle.svg";
+            } else {
+                weather.src = "./weather/cloud.svg";
+            }
+            city.innerHTML = data.name;
+            temp.innerHTML = Math.round(data.main.temp) + "°C";
+            humidity.innerHTML = data.main.humidity + "%";
+            wind.innerHTML = data.wind.speed + " km/h";
+        })
+        .catch(error => {
+            weather.src = "./weather/umbrella.svg";
+            city.innerHTML = "Unknown Place / Not a City";
+            alert("Invalid City Name" + `\n` + "You entered the wrong city name" + `\n` + "Please try again!");
+            console.error('Error fetching weather data:', error);
+        });
 }
 
 // call function
@@ -55,7 +58,9 @@ searchBtn.addEventListener("click", () => {
 	temp.innerHTML = "0°C";
 	humidity.innerHTML = "0%";
 	wind.innerHTML = "0.00 km/h";
-	checkWeather(searchBox.value);
+	setTimeout(() => {
+        checkWeather(searchBox.value);
+    }, 1000);
 });
 
 // keyboard accessibility logic
